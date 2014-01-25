8aed2196dd822b17fc60bfb0f57f039febce71db
Shaun <shaunktw@gmail.com>
2014-01-25 12:08:10 -0500

    added tests to test for floating point inputs

--- discount_calculator_test.py
+++ discount_calculator_test.py
@@ -20,3 +20,14 @@ class DiscountCalculatorTests(unittest.TestCase):
 	def invalid_discount_type_test(self):
 		discount_calculator = DiscountCalculator()
 		self.assertRaises(ValueError, discount_calculator.calculate, 250, 5, 'random')
+
+	def floating_point_percentage_discount_test(self):
+		discount_calculator = DiscountCalculator()
+		result = discount_calculator.calculate(100.0,10.0, 'percent')
+		self.assertEqual(10.0, result)
+
+	def floating_point_absolute_discount_test(self):
+		discount_calculator = DiscountCalculator()
+		result = discount_calculator.calculate(250.0,5.0, 'absolute')
+		self.assertEqual(5.0, result)
+	
